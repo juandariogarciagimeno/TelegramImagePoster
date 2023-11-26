02be8e9a8bbeb3f22f6de5f67ef2d5c1751e6fab
Juanikus <juanikus99@gmail.com>
2023-11-26 15:47:08 +0100

    fix twitter link handling

--- TwitterManager.ts
+++ TwitterManager.ts
@@ -2,7 +2,8 @@ import { Scraper, Tweet } from '@the-convocation/twitter-scraper';
 import { TwitterImage } from './TwitterImage.js';
 
 export class TwitterManager {
-    private static UrlRegex = new RegExp("http(?:s)?:\/\/(?:www\.)?(twitter|x)\.com\/(?<user>[a-zA-Z0-9_]+)\/status\/(?<id>.*)");
+    private static UrlRegex = /http(?:s)?:\/\/(?:www\.)?(fx)?(twitter|x)\.com\/(?<user>[a-zA-Z0-9_]+)\/status\/(?<id>.*)/
+    private static IdExtract = /\/status\/(\d+)/;
     private static scrapper = new Scraper({fetch:fetch});
 
     public static IsTwitterURL(url:string):boolean {
@@ -11,12 +12,13 @@ export class TwitterManager {
     }
 
     public static async GetTweetImages (url:string): Promise<TwitterImage> {
-        var tweetid = this.UrlRegex.exec(url).groups["id"];
+        var match = this.IdExtract.exec(url);
+        var tweetid = match[1];
         tweetid = tweetid.split('?')[0];
         var tweet = await this.scrapper.getTweet(tweetid);
 
         var imgurls = tweet.photos.map(x => x.url);
 
-        return new TwitterImage(url, imgurls, tweet.username);
+        return new TwitterImage(tweet.permanentUrl ?? url, imgurls, tweet.username);
     }
 }
